70a5d3f3ee468910583533a1b2d5a31d456188a0
Mayo <memberwws@gmail.com>
2021-01-13 00:05:01 +0900

    ナビゲーションメニューを作成

--- backend/resources/views/layouts/app.blade.php
+++ backend/resources/views/layouts/app.blade.php
@@ -25,15 +25,19 @@
             @livewire('navigation-menu')
 
             <!-- Page Heading -->
-            <header class="bg-white shadow">
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    {{ $header }}
-                </div>
-            </header>
+            @auth
+                <header class="bg-white shadow">
+                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                        {{ $header }}
+                    </div>
+                </header>
+            @endauth
 
             <!-- Page Content -->
             <main>
-                {{ $slot }}
+                @auth
+                    {{ $slot }}
+                @endauth
             </main>
         </div>
 
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('dashboard');
+    return view('layouts.app');
 });
 
 // ポスト一覧画面表示
